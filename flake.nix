64efe5ff13db56fbf72382b86e9e41ce459f509e
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-05-01 20:52:22 +0300

    build(nix): remove useless inputs, use crate2nix build platform

--- flake.nix
+++ flake.nix
@@ -1,30 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    naersk = {
-      url = "github:yusdacra/naersk/feat/cargolock-git-deps";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    rustOverlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nixCargoIntegration = {
       url = "github:yusdacra/nix-cargo-integration";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.naersk.follows = "naersk";
-      inputs.rustOverlay.follows = "rustOverlay";
     };
   };
 
   outputs = inputs: inputs.nixCargoIntegration.lib.makeOutputs {
     root = ./.;
-    overrides = {
-      build = common: prevb: {
-        allRefs = true;
-        gitSubmodules = true;
-        nativeBuildInputs = prevb.nativeBuildInputs ++ [ common.pkgs.rustfmt ];
-      };
-    };
+    buildPlatform = "crate2nix";
   };
 }
