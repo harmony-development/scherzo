b604037bd6858318ed5a4d9b2365b692927aaaa4
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-06-17 02:05:56 +0300

    chore(deps): update flake deps

--- flake.nix
+++ flake.nix
@@ -15,18 +15,7 @@
     root = ./.;
     buildPlatform = "crate2nix";
     overrides = {
-      pkgs = common: prev: {
-        overlays = [
-          (final: prev: {
-            llvmPackages_12 = prev.llvmPackages_12 // {
-              clang = prev.lib.hiPrio prev.llvmPackages_12.clang;
-              bintools = prev.lib.setPrio (-20) prev.llvmPackages_12.bintools;
-            };
-          })
-        ] ++ prev.overlays;
-      };
       shell = common: prev: {
-        packages = prev.packages ++ [ common.pkgs.mkcert ];
         commands = prev.commands ++ [
           {
             name = "generate-cert";
