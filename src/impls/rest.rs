6a0281ca6708ab62d5a720486aeb34a09f6834f5
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-06-20 18:21:10 +0300

    perf(rest): dont load a file into memory, turn it into a stream and directly pass it as a response body

--- src/impls/rest.rs
+++ src/impls/rest.rs
@@ -5,21 +5,35 @@ use crate::{
 };
 
 use std::{
+    cmp,
     collections::HashMap,
+    fs::Metadata,
     path::{Path, PathBuf},
+    pin::Pin,
     str::FromStr,
     sync::Arc,
+    task::Poll,
 };
 
 use futures_util::StreamExt;
 use harmony_rust_sdk::api::{
     exports::{
-        hrpc::{futures_util, server::ServerError as HrpcError, warp},
-        prost::bytes::Buf,
+        hrpc::{
+            futures_util::{
+                self,
+                future::{self, Either},
+                ready, stream, FutureExt, Stream,
+            },
+            server::ServerError as HrpcError,
+            warp,
+        },
+        prost::bytes::{Buf, Bytes, BytesMut},
     },
     rest::{extract_file_info_from_download_response, FileId},
 };
 use reqwest::{header::HeaderValue, StatusCode};
+use tokio::io::{AsyncBufReadExt, AsyncSeekExt, BufReader};
+use tokio_util::io::poll_read_buf;
 use tracing::info;
 use warp::{filters::multipart::*, filters::BoxedFilter, reply::Response, Filter, Reply};
 
@@ -69,23 +83,33 @@ pub fn download(media_root: Arc<PathBuf>) -> BoxedFilter<(impl Reply,)> {
                             .map_err(reject)?
                             .error_for_status()
                             .map_err(reqwest_or_404)?;
-                        let filename = resp
-                            .url()
-                            .path_segments()
-                            .expect("cannot be a cannot-be-a-base url")
-                            .last()
-                            .unwrap_or("unknown");
-                        let disposition = unsafe { disposition_header(filename) };
                         let content_type = get_mimetype(&resp);
-                        let content_type = content_type
+
+                        if let Some(content_type) = content_type
                             .starts_with("image")
-                            .then(|| content_type.parse().unwrap());
-                        let data_stream = resp.bytes_stream();
-                        if let Some(content_type) = content_type {
+                            .then(|| content_type.parse().unwrap())
+                        {
+                            let filename = resp
+                                .url()
+                                .path_segments()
+                                .expect("cannot be a cannot-be-a-base url")
+                                .last()
+                                .unwrap_or("unknown");
+                            let disposition = unsafe { disposition_header(filename) };
+                            let len = resp
+                                .headers()
+                                .get(&http::header::CONTENT_LENGTH)
+                                .map(|h| h.to_str().ok())
+                                .flatten()
+                                .map(|s| s.parse::<u64>().ok())
+                                .flatten()
+                                .unwrap_or(0);
+                            let data_stream = resp.bytes_stream();
                             Ok((
                                 disposition,
                                 content_type,
                                 warp::hyper::Body::wrap_stream(data_stream),
+                                len,
                             ))
                         } else {
                             Err(reject(ServerError::NotAnImage))
@@ -111,11 +135,20 @@ pub fn download(media_root: Arc<PathBuf>) -> BoxedFilter<(impl Reply,)> {
                                     (unsafe { disposition_header(name) }, mimetype.clone())
                                 })
                                 .map_err(|e| reject(ServerError::Unexpected(e.to_string())))?;
+                        let len = resp
+                            .headers()
+                            .get(&http::header::CONTENT_LENGTH)
+                            .map(|h| h.to_str().ok())
+                            .flatten()
+                            .map(|s| s.parse::<u64>().ok())
+                            .flatten()
+                            .unwrap_or(0);
                         let data_stream = resp.bytes_stream();
                         Ok((
                             disposition,
                             mimetype,
                             warp::hyper::Body::wrap_stream(data_stream),
+                            len,
                         ))
                     }
                     FileId::Id(id) => {
@@ -126,13 +159,24 @@ pub fn download(media_root: Arc<PathBuf>) -> BoxedFilter<(impl Reply,)> {
             }
         })
         .map(
-            |(disposition, content_type, data): (HeaderValue, HeaderValue, warp::hyper::Body)| {
+            |(disposition, content_type, data, len): (
+                HeaderValue,
+                HeaderValue,
+                warp::hyper::Body,
+                u64,
+            )| {
                 let mut resp = Response::new(data);
                 resp.headers_mut()
                     .insert(http::header::CONTENT_TYPE, content_type);
                 // TODO: content disposition attachment thingy?
                 resp.headers_mut()
                     .insert(http::header::CONTENT_DISPOSITION, disposition);
+                resp.headers_mut()
+                    .insert(http::header::CONTENT_LENGTH, unsafe {
+                        HeaderValue::from_maybe_shared_unchecked(Bytes::from(
+                            len.to_string().into_bytes(),
+                        ))
+                    });
                 resp
             },
         )
@@ -210,46 +254,139 @@ fn reject(err: impl Into<ServerError>) -> warp::Rejection {
 
 // Safety: the `name` argument MUST ONLY contain ASCII characters.
 unsafe fn disposition_header(name: &str) -> HeaderValue {
-    HeaderValue::from_maybe_shared_unchecked(format!("attachment; filename={}", name).into_bytes())
+    HeaderValue::from_maybe_shared_unchecked(Bytes::from(
+        format!("attachment; filename={}", name).into_bytes(),
+    ))
 }
 
 async fn get_file(
     media_root: &Path,
     id: &str,
-) -> Result<(HeaderValue, HeaderValue, warp::hyper::Body), ServerError> {
-    match tokio::fs::read(media_root.join(id)).await {
-        Ok(mut raw) => {
-            let mut pos = raw.iter().enumerate().filter(|(_, b)| **b == SEPERATOR);
-            let filename_sep = pos.next().unwrap().0;
-            let mimetype_sep = pos.next().unwrap().0;
-            drop(pos);
-
-            let (first, rest) = raw.split_at(filename_sep);
-            let disposition = unsafe {
-                let filename = std::str::from_utf8_unchecked(first);
-                // Safety: filenames must be valid ASCII characters since we get them through only ASCII allowed structures [ref:ascii_filename_upload]
-                disposition_header(filename)
-            };
-            let mimetype = {
-                let mimetype = rest
-                    .split_at(mimetype_sep - filename_sep)
-                    .0
-                    .split_at(1)
-                    .1
-                    .to_vec();
-                // Safety: mimetypes must be valid ASCII chars since we get them through only ASCII allowed structures [ref:ascii_mimetype_upload]
-                unsafe { HeaderValue::from_maybe_shared_unchecked(mimetype) }
-            };
-            drop(raw.drain(0..=mimetype_sep));
-
-            Ok((disposition, mimetype, raw.into()))
+) -> Result<(HeaderValue, HeaderValue, warp::hyper::Body, u64), ServerError> {
+    let file_path = media_root.join(id);
+    let mut file = tokio::fs::File::open(file_path).await.map_err(|err| {
+        if let std::io::ErrorKind::NotFound = err.kind() {
+            ServerError::MediaNotFound
+        } else {
+            err.into()
         }
-        Err(err) => {
-            if let std::io::ErrorKind::NotFound = err.kind() {
-                Err(ServerError::MediaNotFound)
-            } else {
-                Err(err.into())
-            }
+    })?;
+    let metadata = file.metadata().await?;
+    let mut buf_reader = BufReader::new(&mut file);
+
+    let mut filename_raw = Vec::with_capacity(20);
+    buf_reader.read_until(SEPERATOR, &mut filename_raw).await?;
+    filename_raw.pop();
+
+    let mut mimetype_raw = Vec::with_capacity(20);
+    buf_reader.read_until(SEPERATOR, &mut mimetype_raw).await?;
+    mimetype_raw.pop();
+
+    let start = (filename_raw.len() + mimetype_raw.len()) as u64 + 2;
+    let end = metadata.len();
+    let mimetype: Bytes = mimetype_raw.into();
+
+    let disposition = unsafe {
+        let filename = std::str::from_utf8_unchecked(&filename_raw);
+        // Safety: filenames must be valid ASCII characters since we get them through only ASCII allowed structures [ref:ascii_filename_upload]
+        disposition_header(filename)
+    };
+    // Safety: mimetypes must be valid ASCII chars since we get them through only ASCII allowed structures [ref:ascii_mimetype_upload]
+    let mimetype = unsafe { HeaderValue::from_maybe_shared_unchecked(mimetype) };
+
+    let buf_size = optimal_buf_size(&metadata);
+    Ok((
+        disposition,
+        mimetype,
+        warp::hyper::Body::wrap_stream(file_stream(file, buf_size, (start, end))),
+        end - start,
+    ))
+}
+
+fn file_stream(
+    mut file: tokio::fs::File,
+    buf_size: usize,
+    (start, end): (u64, u64),
+) -> impl Stream<Item = Result<Bytes, std::io::Error>> + Send {
+    use std::io::SeekFrom;
+
+    let seek = async move {
+        if start != 0 {
+            file.seek(SeekFrom::Start(start)).await?;
         }
+        Ok(file)
+    };
+
+    seek.into_stream()
+        .map(move |result| {
+            let mut buf = BytesMut::new();
+            let mut len = end - start;
+            let mut f = match result {
+                Ok(f) => f,
+                Err(f) => return Either::Left(stream::once(future::err(f))),
+            };
+
+            Either::Right(stream::poll_fn(move |cx| {
+                if len == 0 {
+                    return Poll::Ready(None);
+                }
+                reserve_at_least(&mut buf, buf_size);
+
+                let n = match ready!(poll_read_buf(Pin::new(&mut f), cx, &mut buf)) {
+                    Ok(n) => n as u64,
+                    Err(err) => {
+                        tracing::debug!("file read error: {}", err);
+                        return Poll::Ready(Some(Err(err)));
+                    }
+                };
+
+                if n == 0 {
+                    tracing::debug!("file read found EOF before expected length");
+                    return Poll::Ready(None);
+                }
+
+                let mut chunk = buf.split().freeze();
+                if n > len {
+                    chunk = chunk.split_to(len as usize);
+                    len = 0;
+                } else {
+                    len -= n;
+                }
+
+                Poll::Ready(Some(Ok(chunk)))
+            }))
+        })
+        .flatten()
+}
+
+fn reserve_at_least(buf: &mut BytesMut, cap: usize) {
+    if buf.capacity() - buf.len() < cap {
+        buf.reserve(cap);
     }
 }
+
+fn optimal_buf_size(metadata: &Metadata) -> usize {
+    let block_size = get_block_size(metadata);
+
+    // If file length is smaller than block size, don't waste space
+    // reserving a bigger-than-needed buffer.
+    cmp::min(block_size as u64, metadata.len()) as usize
+}
+
+#[cfg(unix)]
+fn get_block_size(metadata: &Metadata) -> usize {
+    use std::os::unix::fs::MetadataExt;
+    //TODO: blksize() returns u64, should handle bad cast...
+    //(really, a block size bigger than 4gb?)
+
+    // Use device blocksize unless it's really small.
+    cmp::max(metadata.blksize() as usize, DEFAULT_READ_BUF_SIZE)
+}
+
+#[cfg(not(unix))]
+#[inline(always)]
+fn get_block_size(_metadata: &Metadata) -> usize {
+    DEFAULT_READ_BUF_SIZE
+}
+
+const DEFAULT_READ_BUF_SIZE: usize = 8_192;
