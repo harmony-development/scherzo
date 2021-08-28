f052e1efe186cf912bc32932c30c66a240dd0d39
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-08-28 04:02:59 +0300

    refactor: remove migration two

--- src/db/migration.rs
+++ src/db/migration.rs
@@ -1,14 +1,10 @@
-use harmony_rust_sdk::api::exports::{hrpc::encode_protobuf_message, prost::Message};
-
-use std::{convert::TryInto, mem::size_of};
-
-use crate::db::{chat::make_msg_key, Batch};
+use std::convert::TryInto;
 
 use super::{Db, DbResult};
 
 type Migration = fn(&dyn Db) -> DbResult<()>;
 
-pub const MIGRATIONS: [Migration; 2] = [initial_db_version, migrate_contents];
+pub const MIGRATIONS: [Migration; 1] = [initial_db_version];
 
 pub fn get_db_version(db: &dyn Db) -> DbResult<(usize, bool)> {
     let version_tree = db.open_tree(b"version")?;
@@ -49,45 +45,5 @@ fn initial_db_version(db: &dyn Db) -> DbResult<()> {
     if !version_tree.contains_key(b"version")? {
         version_tree.insert(b"version", &0_usize.to_be_bytes())?;
     }
-    Ok(())
-}
-
-fn migrate_contents(db: &dyn Db) -> DbResult<()> {
-    let tree = db.open_tree(b"chat")?;
-    const LEN: usize = make_msg_key(0, 0, 0).len();
-
-    let messages = tree
-        .iter()
-        .filter_map(|res| {
-            let (key, val) = res.unwrap();
-            (key.len() == LEN)
-                .then(|| {
-                    let (gid_raw, rest) = key.split_at(size_of::<u64>());
-                    if rest[0] != 8 {
-                        return None;
-                    }
-                    let gid = u64::from_be_bytes(gid_raw.try_into().ok()?);
-                    let (cid_raw, rest) = rest[1..].split_at(size_of::<u64>());
-                    if rest[0] != 9 {
-                        return None;
-                    }
-                    let cid = u64::from_be_bytes(cid_raw.try_into().ok()?);
-                    let mid = u64::from_be_bytes(rest[1..].try_into().ok()?);
-
-                    let msg = sdk_migration_two::api::harmonytypes::Message::decode(val.as_slice())
-                        .ok()?;
-
-                    Some((make_msg_key(gid, cid, mid), msg))
-                })
-                .flatten()
-        });
-
-    let mut batch = Batch::default();
-    for (key, msg) in messages {
-        let val = encode_protobuf_message(msg);
-        batch.insert(&key, val.as_ref());
-    }
-    tree.apply_batch(batch)?;
-
     Ok(())
 }
