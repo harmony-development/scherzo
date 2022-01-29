bd15a3a8fe12feb3f9d9a9fcea5903f5b9ad1ced
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-01-29 05:02:57 +0300

    Revert "Revert "fix(db/migrations): only check for user profile keys in add account kind""
    This reverts commit cb3bf83a81fcf9a5a3587a2598a4dc20602141f0.

--- src/db/migration/add_account_kind.rs
+++ src/db/migration/add_account_kind.rs
@@ -2,7 +2,10 @@ use std::fmt::Display;
 
 use super::*;
 
-use db::{profile::USER_PREFIX, rkyv_ser, Batch, DbError};
+use db::{
+    profile::{make_user_profile_key, USER_PREFIX},
+    rkyv_ser, Batch, DbError,
+};
 use hrpc::box_error;
 
 pub(super) fn migrate(db: &Db) -> BoxFuture<'_, DbResult<()>> {
@@ -11,21 +14,23 @@ pub(super) fn migrate(db: &Db) -> BoxFuture<'_, DbResult<()>> {
         let mut batch = Batch::default();
         for res in profile_tree.scan_prefix(USER_PREFIX).await {
             let (key, val) = res?;
-            let old_profile = rkyv::from_bytes::<profile::Profile>(&val);
-            if let Ok(old_profile) = &old_profile {
-                let new_val = rkyv_ser(old_profile);
-                batch.insert(key, new_val);
-            } else if rkyv::check_archived_root::<crate::api::profile::Profile>(&val).is_ok() {
-                // if it's new, then its already fine
-                continue;
-            } else {
-                old_profile.map_err(|err| DbError {
-                    inner: box_error(AnyhowError(anyhow::anyhow!(
-                        "profile with key {} has invalid state: {}",
-                        String::from_utf8_lossy(key.as_ref()),
-                        err
-                    ))),
-                })?;
+            if key.len() == make_user_profile_key(0).len() {
+                let old_profile = rkyv::from_bytes::<profile::Profile>(&val);
+                if let Ok(old_profile) = &old_profile {
+                    let new_val = rkyv_ser(old_profile);
+                    batch.insert(key, new_val);
+                } else if rkyv::check_archived_root::<crate::api::profile::Profile>(&val).is_ok() {
+                    // if it's new, then its already fine
+                    continue;
+                } else {
+                    old_profile.map_err(|err| DbError {
+                        inner: box_error(AnyhowError(anyhow::anyhow!(
+                            "profile with key {} has invalid state: {}",
+                            String::from_utf8_lossy(key.as_ref()),
+                            err
+                        ))),
+                    })?;
+                }
             }
         }
         profile_tree.apply_batch(batch).await?;
