a229f73b30c0bd83ee020d6b8ed3c6e829d4e764
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-04-02 23:15:38 +0300

    chore: update flake deps, format nix files

--- flake.nix
+++ flake.nix
@@ -11,40 +11,44 @@
     };
   };
 
-  outputs = inputs: inputs.nixCargoIntegration.lib.makeOutputs {
-    root = ./.;
-    overrides = {
-      crateOverrides = common: _: {
-        mediasoup-sys = prev:
-          let
+  outputs = inputs:
+    inputs.nixCargoIntegration.lib.makeOutputs {
+      root = ./.;
+      overrides = {
+        crateOverrides = common: _: {
+          mediasoup-sys = prev: let
             pkgs = common.pkgs;
-            pythonPkgs = pkgs: with pkgs; [
-              pip
-            ];
+            pythonPkgs = pkgs:
+              with pkgs; [
+                pip
+              ];
             pythonWithPkgs = pkgs.python3.withPackages pythonPkgs;
-            all = (with pkgs; [ cmake gnumake nodejs meson ninja ]) ++ [ pythonWithPkgs ];
-          in
-          {
-            buildInputs = (prev.buildInputs or [ ]) ++ all;
-            nativeBuildInputs = (prev.nativeBuildInputs or [ ]) ++ all;
+            all = (with pkgs; [cmake gnumake nodejs meson ninja]) ++ [pythonWithPkgs];
+          in {
+            buildInputs = (prev.buildInputs or []) ++ all;
+            nativeBuildInputs = (prev.nativeBuildInputs or []) ++ all;
           };
-      };
-      shell = common: prev: {
-        packages = prev.packages ++ (with common.pkgs; [
-          mold
-          mkcert
-        ]);
-        commands = prev.commands ++ [
-          {
-            name = "generate-cert";
-            command = ''
-              mkcert localhost 127.0.0.1 ::1
-              mv localhost+2.pem cert.pem
-              mv localhost+2-key.pem key.pem
-            '';
-          }
-        ];
+        };
+        shell = common: prev: {
+          packages =
+            prev.packages
+            ++ (with common.pkgs; [
+              mold
+              mkcert
+            ]);
+          commands =
+            prev.commands
+            ++ [
+              {
+                name = "generate-cert";
+                command = ''
+                  mkcert localhost 127.0.0.1 ::1
+                  mv localhost+2.pem cert.pem
+                  mv localhost+2-key.pem key.pem
+                '';
+              }
+            ];
+        };
       };
     };
-  };
 }
